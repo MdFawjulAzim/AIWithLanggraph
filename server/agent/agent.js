0d1d061416e53640da5f8d49e6eab66fc8286d20
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2026-02-24 00:17:56 +0600

    Implement tools.

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -1,20 +1,37 @@
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatAnthropic } from "@langchain/anthropic";
 
+const weatherTool = tool(
+  async ({ query }) => {
+    console.log("query", query);
+
+    // TODO: Implement the weather tool by fetching an API
+
+    return "The weather in Tokyo is sunny.";
+  },
+  {
+    name: "weather",
+    description: "Get the weather in a given location",
+    schema: z.object({
+      query: z.string().describe("The query to use in search"),
+    }),
+  },
+);
+
 const model = new ChatAnthropic({
   model: "claude-3-5-sonar-latest",
 });
 
 const agent = createReactAgent({
   llm: model,
-  tools: [],
+  tools: [weatherTool],
 });
 
 const result = await agent.invoke({
   messages: [
     {
       role: "user",
-      content: "Hello, How can you help me today?",
+      content: "What's the weather in Tokyo?",
     },
   ],
 });
